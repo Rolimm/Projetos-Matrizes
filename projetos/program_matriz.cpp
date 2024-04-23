00c9ffb2bbdc5aca745d268e551318ea9c3cbe8f
Gabriel Rolim <gabriel.rolim@crossdreams.com.br>
2024-04-23 00:22:47 -0300

    alteraçoes de bugs

--- projetos/program_matriz.cpp
+++ projetos/program_matriz.cpp
@@ -8,7 +8,7 @@ int main() {
 	printf("Preciso que voce digite 4 numeros pares abaixo de 11 e 4 numeros impares acima de 11,\nCaso contrario ira dar erro\n\n");
 	for(i=0;i<=1;i++){
 		for(j=0;j<=3;j++){
-				printf("Digite um numero par:\n");
+				printf("Digite um numero\n");
 		        scanf("%d",&matriz[i][j]);	
 
 		        if(matriz[i][j]==0 || matriz[i][j]>=21){
@@ -33,6 +33,18 @@ int main() {
 		
         printf("\nparabens voce respeitou minhas regras\n");
         
+
+   
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 4; j++) {
+                if ((i == 0 && matriz[i][j] % 2 != 0) || (i == 1 && matriz[i][j] % 2 == 0)) {
+                    aux = matriz[0][j];
+                    matriz[0][j] = matriz[1][j];
+                    matriz[1][j] = aux;
+                }
+            }
+        }
+        
         while(parar){
             parar = 0;
                 for (i = 0; i < 2; i++) {
@@ -45,16 +57,6 @@ int main() {
                         }
                     }
                 }
-        }
-   
-        for (i = 0; i < 2; i++) {
-            for (j = 0; j < 4; j++) {
-                if ((i == 0 && matriz[i][j] % 2 != 0) || (i == 1 && matriz[i][j] % 2 == 0)) {
-                    aux = matriz[0][j];
-                    matriz[0][j] = matriz[1][j];
-                    matriz[1][j] = aux;
-                }
-            }
         }
 			
 		printf("\nAqui está a primeira matriz:\n");	
@@ -74,6 +76,7 @@ int main() {
                 }
             }
         }
+    
         printf("\nAqui está a segunda matriz:\n");
         for (i = 0; i < 2; i++) {
             for (j = 0; j < 4; j++) {
